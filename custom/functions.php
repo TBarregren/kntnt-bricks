54874923d97e2b072aac1a39477bf59b0d7a6eef
Thomas Barregren <thomas@kntnt.com>
2021-10-26 16:01:21 +0200

    Removed functions.php.

--- custom/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-add_filter( 'kntnt-bricks-breakpoints', function ( $breakpoints ) {
-	return [
-		'tablet_portrait'  => 1023,
-		'mobile_landscape' => 767,
-		'mobile_portrait'  => 479,
-	];
-} );
